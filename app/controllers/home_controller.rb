135358f53ff675461788b8c0a2fc6b79184eaa09
Krzysztof Hasiński <krzysztof.hasinski@gmail.com>
2024-01-30 20:36:13 +0100

    Removed unimplemented scopes from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,6 @@ class HomeController < ApplicationController
 
     @pets = {
       recently_added: Pet.all.order(created_at: :desc).limit(list_size),
-      most_commented: Pet.all.order(created_at: :desc).limit(list_size), # TODO: add comments
-      most_liked: Pet.all.order(created_at: :desc).limit(list_size), # TODO: add likes
       recently_adopted: Pet.all.order(adoption_date: :desc).limit(list_size),
       random: Pet.all.order(Arel.sql('RANDOM()')).limit(list_size),
       most_viewed: Pet.all.most_viewed.limit(list_size),
